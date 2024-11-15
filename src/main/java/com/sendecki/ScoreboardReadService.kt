47a213dbafa6491b3a3ba2537cea1fd2c35faf1e
lsendecki <lsendecki@gmail.com>
2024-11-15 12:01:40 +0100

    unused import

--- src/main/java/com/sendecki/ScoreboardReadService.kt
+++ src/main/java/com/sendecki/ScoreboardReadService.kt
@@ -1,7 +1,5 @@
 package com.sendecki
 
-import java.util.PriorityQueue
-
 class ScoreboardReadService(private val scoreboardRepo: ScoreboardRepo) {
 
     fun getSummary() = scoreboardRepo.getStorage()
